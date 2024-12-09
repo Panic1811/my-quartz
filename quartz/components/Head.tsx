ede55f52e8b89c5137dc0b9accadf2fc4ba4d73b
Panic1811 <56132791+Panic1811@users.noreply.github.com>
2024-12-10 03:45:21 +0400

    Quartz sync: Dec 10, 2024, 3:45 AM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -12,7 +12,7 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon.ico")
+    const iconPath = joinSegments(baseDir, "static/vb favicon.ico")
     const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
