c5350910a27a463ee9dc0e74f8d28685a528f4cd
Panic1811 <56132791+Panic1811@users.noreply.github.com>
2024-12-10 02:22:53 +0400

    Quartz sync: Dec 10, 2024, 2:22 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,7 +6,7 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={`page-title ${displayClass ?? ""}`}>
-      <img style="height: 1.2em; margin: 0 0.4em 0 0; vertical-align: text-bottom;" src="https://github.com/microsoft/fluentui-emoji/raw/main/assets/Card%20file%20box/Color/card_file_box_color.svg"></img>
+      <img style="height: 1.2em; margin: 0 0.4em 0 0; vertical-align: text-bottom;" src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/refs/heads/main/assets/Notebook/Color/notebook_color.svg"></img>
       <a href={baseDir}>{title}</a>
     </h1>
   )
