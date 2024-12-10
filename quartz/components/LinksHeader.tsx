573d8dbb2e73b88bb7bfd682874cb51230d1812e
Panic1811 <56132791+Panic1811@users.noreply.github.com>
2024-12-11 03:10:59 +0400

    Quartz sync: Dec 11, 2024, 3:10 AM

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -16,7 +16,7 @@ export default (() => {
           </span>
           <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Brain/Color/brain_color.svg"></img>
-            <a href="https://vedantbali.com/Dictionary/Dictionary-Glossary">Learn</a>
+            <a href="https://vedantbali.com/Knowledge/Knowledge-Glossary">Learn</a>
           </span>
           <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/refs/heads/main/assets/Movie%20camera/Color/movie_camera_color.svg"></img>
